a54a9a9f0dc5e3b5e3a68c1c19864ff88b84c3d5
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-08-10 19:05:07 +0800

    更改jmc主页

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
       <v-container class="py-0 fill-height">
-        <div class="text-center">
+        <div class="text-center mr-5">
           <div class="text-h6">JerryMc</div>
         </div>
 
@@ -13,7 +13,7 @@
         <v-spacer></v-spacer>
 
         <div class="text-center">
-          <v-chip class="ma-2" color="indigo" text-color="white">
+          <v-chip class="ma-1" color="indigo" text-color="white">
             <v-avatar class="mr-2" color="grey darken-1" size="24"></v-avatar>
             {{ isLogin ? accountInfo.name : '未登录' }}
           </v-chip>
@@ -41,6 +41,12 @@ export default {
       accoutInfo: {
         name: ''
       }
+    }
+  },
+  mounted(){
+    if(window.screen.height <= 450){
+      console.log('屏幕太小！');
+      throw new Error('屏幕太小！');
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,27 @@
       <v-img lazy-src="https://s2.loli.net/2022/08/10/sQG8KqyVhUzn13I.png" :max-height="screen.height"
         :max-width="screen.width" src="/main_page_img.png" style="position:absolute;z-index: 1;"></v-img>
       <v-scroll-y-reverse-transition>
-        <div v-if="show" class="text-center main" :style="(()=>{return `margin-top:${mainMarginTop}}px`})()">
-          <h1>欢迎来到 JerryMc</h1>
+        <div v-if="show" class="text-center main" ref="main">
+          <h1 class="mtitle mb-10">JerryMc</h1>
+          <h5 class="mdescription mb-6">JerryMc (简称jmc) 是一个养老的生存服务器, 玩家活跃, 等待你的加入哦~</h5>
+          <div class="d-flex justify-space-around mb-6 btn-group">
+            <div>
+              <v-btn block>
+                <v-icon>mdi-account-circle-outline</v-icon> 登录/注册
+              </v-btn>
+            </div>
+            <div>
+              <v-btn block dark color="green lighten-1">
+                <v-icon>mdi-text-box-check-outline</v-icon> 申请白名单
+              </v-btn>
+            </div>
+            <div>
+              <v-btn block outlined color="blue lighten-1" :loading="loading.qq"
+                @click="openNewBlank('https://qm.qq.com/cgi-bin/qm/qr?k=cCRVoxnZBl6T2vVpSHRnUk1f3RR3RJH9')">
+                <v-icon>mdi-qqchat</v-icon> 加入QQ群
+              </v-btn>
+            </div>
+          </div>
         </div>
       </v-scroll-y-reverse-transition>
     </div>
@@ -22,7 +41,10 @@ export default {
         height: 1000
       },
       show: false,
-      mainMarginTop:500
+      mainMarginTop: 500,
+      loading: {
+        qq: false
+      }
     }
   },
   mounted() {
@@ -33,6 +55,16 @@ export default {
     setTimeout(() => {
       that.show = true;
     }, 700);
+  },
+  methods: {
+    openNewBlank(url) {
+      var that = this;
+      this.loading.qq = true;
+      window.open(url, '_blank');
+      setTimeout(() => {
+        that.loading.qq = false;
+      }, 200);
+    }
   }
 }
 </script>
@@ -42,5 +74,25 @@ export default {
   z-index: 2;
   left: 0;
   right: 0;
+  margin-top: 350px;
+  color: #FFF3E0;
+}
+
+.main .mtitle {
+  font-family: "Roboto", sans-serif !important;
+  font-weight: 500;
+  font-size: 100px;
+  line-height: 2rem;
+  letter-spacing: 0.0125em !important;
+  user-select: none;
+}
+
+.main .btn-group {
+  margin-left: 25%;
+  margin-right: 25%;
+}
+
+.main .btn-group div {
+  width: 25%;
 }
 </style>.
