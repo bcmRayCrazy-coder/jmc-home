be042e4699132e94014f8eae8aa5c4ef19d93609
ItzJerry317 <85010676+ItzJerry317@users.noreply.github.com>
2022-08-26 19:36:56 +0800

    二周目网站更新

--- pages/index.vue
+++ pages/index.vue
@@ -2,71 +2,41 @@
   <div>
     <v-snackbar v-model="snackbar">
       {{ text }}
-      <v-btn
-        style="position: relative; margin-left: 20vw"
-        color="pink"
-        text
-        @click="snackbar = false"
-      >
+      <v-btn style="position: relative; margin-left: 20vw" color="pink" text @click="snackbar = false">
         我知道了
       </v-btn>
     </v-snackbar>
     <div class="main-screen" ref="mainWrapper">
-      <v-img
-        :lazy-src="mainImgLazy"
-        :height="screen.height"
-        :width="screen.width"
-        :src="mainImg"
-        style="position: absolute; z-index: 1"
-      ></v-img>
+      <v-img :lazy-src="mainImgLazy" :height="screen.height" :width="screen.width" :src="mainImg"
+        style="position: absolute; z-index: 1"></v-img>
       <v-scroll-y-reverse-transition>
         <div class="text-center main" ref="main">
-          <h1 class="mtitle mb-10">JerryMC</h1>
+          <h1 class="mtitle mb-10">JerryMC | 二周目</h1>
           <h5 class="mdescription mb-6">
-            JerryMC (简称JMC) 是一个养老的生存服务器, 玩家活跃, 等待你的加入哦~
+            JerryMC (简称JMC) 是一个养老的生存服务器, 玩家活跃, 等待你的加入哦~<br>一周目存档下载请入qq群
           </h5>
           <div class="d-flex mb-6 btn-group buttons">
             <div>
-              <v-btn
-                v-if="isLogin == false"
-                block
-                @click="goto('/users/login')"
-                id="login"
-              >
+              <v-btn v-if="isLogin == false" block @click="goto('/users/login')" id="login">
                 <v-icon>mdi-account-circle-outline</v-icon> 登录/注册
               </v-btn>
-              <v-btn
-                v-if="isLogin == true"
-                block
-                @click="goto('/users/login')"
-                id="login"
-              >
+              <v-btn v-if="isLogin == true" block @click="goto('/users/login')" id="login">
                 <v-icon>mdi-account-box-multiple-outline</v-icon> 切换账号
               </v-btn>
             </div>
             <v-spacer></v-spacer>
             <div>
-              <v-btn
-                block
-                dark
-                color="green lighten-1"
-                @click="goto('/whitelist/request')"
-              >
+              <v-btn block dark color="green lighten-1" @click="goto('/whitelist/request')">
                 <v-icon>mdi-text-box-check-outline</v-icon> 申请白名单
               </v-btn>
             </div>
             <v-spacer></v-spacer>
             <div>
-              <v-btn
-                block
-                color="blue lighten-1"
-                :loading="loading.qq"
-                @click="
-                  openNewBlank(
-                    'https://qm.qq.com/cgi-bin/qm/qr?k=cCRVoxnZBl6T2vVpSHRnUk1f3RR3RJH9'
-                  )
-                "
-              >
+              <v-btn block color="blue lighten-1" :loading="loading.qq" @click="
+                openNewBlank(
+                  'https://qm.qq.com/cgi-bin/qm/qr?k=cCRVoxnZBl6T2vVpSHRnUk1f3RR3RJH9'
+                )
+              ">
                 <v-icon>mdi-qqchat</v-icon> 加入QQ群
               </v-btn>
             </div>
@@ -82,7 +52,7 @@ export default {
   // https://s2.loli.net/2022/08/11/gdL2MxBvorJehDZ.png
   name: 'IndexPage',
   data() {
-    var mainImg = this.$_.sample(['0.png', '1.png']);
+    var mainImg = this.$_.sample(['7.png', '8.png', '9.png', '10.png']);
     var mainImgLazyMap = {
       '0.png': 'https://s2.loli.net/2022/08/10/sQG8KqyVhUzn13I.png',
       '1.png': 'https://s2.loli.net/2022/08/11/gdL2MxBvorJehDZ.png',
