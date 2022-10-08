6a19b928b9a5e5ccc4089621f4d10164c75bd763
ItzJerry317 <85010676+ItzJerry317@users.noreply.github.com>
2022-10-08 22:08:39 +0800

    Update user.vue

--- pages/tools/user.vue
+++ pages/tools/user.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <h1 align="center">用户管理</h1>
-    <v-text-field :label="用户id"></v-text-field>
+    <v-data-table
+        :search="search"
+        :headers="headers"
+        :items="items"
+        class="data-table"
+        :loading="listLoading"
+      >
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="getStatusColor(item.status)" dark>
+            {{ getStatusText(item.status) }}
+          </v-chip>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            @click="editItem(item)"
+            v-if="userData.isAdmin"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+            class="mr-2"
+            @click="deleteItem(item)"
+            v-if="item.uid == userData.id"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
     <v-select :items="levels" label="管理员等级" solo></v-select>
   </div>
 </template>
@@ -14,7 +44,9 @@ export default {
   },
   data() {
     return {
-      
+      userData: {},
+      search: '',
+      listLoading: true,
       levels: [
         '1',
         '2',
